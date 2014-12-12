7dfe6d531d22fa52726f9ac839fed9f90ff74a4f
ardila <ardila@mit.edu>
2014-12-12 13:16:53 -0500

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +9,21 @@ CHROME_PATH = '/Users/ardila/src/tdd_tutorial/chromedriver'
 print super(LiveServerTestCase)
 
 class NewVisitorTest(StaticLiveServerTestCase):
-        
+
+        @classmethod
+        def setUpClass(cls):
+            for arg in sys.argv:
+                if 'liveserver' in arg:
+                    cls.server_url = 'http://' + arg.split('=')[1]
+                    return
+            super(StaticLiveServerTestCase, cls).setUpClass()
+            cls.server_url = cls.live_server_url
+
+        @classmethod
+        def tearDownClass(cls):
+            if cls.server_url == cls.live_server_url:
+                super(StaticLiveServerTestCase, cls).tearDownClass()
+
         def setUp(self):
             self.browser = webdriver.Chrome(CHROME_PATH)
             self.browser.implicitly_wait(3)
@@ -23,7 +38,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         def test_layout_and_styling(self):
             # Edith goes to the home page
-            self.browser.get(self.live_server_url)
+            self.browser.get(self.server_url)
             self.browser.set_window_size(1024, 768)
 
             inputbox = self.browser.find_element_by_id('id_new_item')
@@ -44,7 +59,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         def test_can_start_a_list_and_retrieve_it_later(self):
             # Bob has heard about a cool new online to-do app. He goes to check out 
             # its homepage
-            self.browser.get(self.live_server_url)
+            self.browser.get(self.server_url)
 
             
             #He notices the page title and header mention to-do lists
